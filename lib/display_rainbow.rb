b87b34ede6d859665b174c0d036d3090964ba595
Pablo Rocha Moreira <prm.gredes@gmail.com>
2017-10-14 23:40:48 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,10 @@
 # Write your #display_rainbow method here
 def display_rainbow(arr)
   t = []
-  arr
+  arr.sort!
   arr.each do |item|
     t << "#{item[0].upcase}: #{item}"
   end
   t = t.join(", ")
-  puts t
+  print t + "\n"
 end
